31c93279751a5a7012be96a3dfbf031f9bed5695
pj-hewakaluge <pankaja.hewakaluge@mail.mcgill.ca>
2025-07-07 13:57:56 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true
+  },
   // If your repo is not at the root domain, set the basePath and assetPrefix:
-  basePath: '/portfolio-site',
-  assetPrefix: '/portfolio-site/',
+  // basePath: '/portfolio-site',
+  // assetPrefix: '/portfolio-site/',
 };
 
 export default nextConfig;
